975bbeec9b7eb3173b2fe4509cfc2a601e4d0b2a
BT_1N3_30 <cpolisi6526@bths.edu>
2018-11-27 08:40:27 -0500

    1

--- src/com/company/faceDrawing.java
+++ src/com/company/faceDrawing.java
@@ -1,4 +1,28 @@
 package com.company;
 
 public class faceDrawing {
+    private String[][] faceFeat;
+    public faceDrawing(String[][] faceFeat){
+        this.faceFeat= faceFeat;
+    }
+    public void fill(String str){
+        for (int col= 0; col<faceFeat[col].length; col++  ){
+            for (int row=0; row<faceFeat.length; row++){
+                faceFeat[row][col]= str;
+            }
+        }
+    }
+
+    public void edit(String replace, int row, int col){
+        faceFeat[row][col]=replace;
+    }
+
+    public String toString{
+        String face= "";
+        for (int col= 0; col<faceFeat[col].length; col++  ){
+            for (int row=0; row<faceFeat.length; row++) {
+                face = face + faceFeat[row][col] + " ";
+            }
+            face= face + /n;
+    }
 }
